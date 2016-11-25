71243c4f3e2f38127f035f073d3f9995d07879df
KarenZC <A01411417@itesm.mx>
2016-11-24 22:32:00 -0600

    Update HistorialVentas.java

--- src/finalprojct/HistorialVentas.java
+++ src/finalprojct/HistorialVentas.java
@@ -1,33 +1,34 @@
-
 package finalprojct;
-
 import java.util.ArrayList;
-
+//Contiene los datos de las ventas de todo el dia
 public class HistorialVentas {
     // Propiedades
+    //Arraylist puede contener objetos de cualquier tipo
     private ArrayList<Venta> historial;
+    //En esta caso es un array de tipo venta y se llama historial
     //Constructor
     public HistorialVentas (){
         this.historial = new ArrayList();
     }
     //agrega ventas
-    public void agregarVenta (Venta v) {
-        this.historial.add(v);
+    public void agregarVenta (Venta v) {//add es un metodo de arraylist, se utiliza para añadir
+        this.historial.add(v);//en este caso al historial
     }
     //Optiene una venta basado en un index
-    public Venta getVentaAt (int index ) {
-        return this.historial.get(index);
+    public Venta getVentaAt (int index ) {//.get es un metodo, 
+        //se utiliza para devolver el elemento que esta en la posicion indicada
+        return this.historial.get(index);//en este caso index
     }
     // optiene el tamaño del historial ventas
-    public int size(){
-        return this.historial.size();
+    public int size(){//.size es un metodo, se utiliza para devolver el numero de elementos
+        return this.historial.size();//en este caso el numero de ventas
     }
     // revisa si el historial de ventas esta vacio
-    public boolean isEmpty() {
-        return this.historial.isEmpty();
+    public boolean isEmpty() {//.isEmpty es un metodo
+        return this.historial.isEmpty();//se utiliza para verificar si la lista tiene elementos
     }
     // resetea el historial de ventas
-    public void clear(){
-        this.historial.clear();
+    public void clear(){//.clear es un metodo
+        this.historial.clear();//se utiliza para eliminar todo los elementos
     }
 }
